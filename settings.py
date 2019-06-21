00fc1bcdf1e9540870295269717a31908e473827
Daniel Malik <mail@fronbasal.de>
2019-06-21 21:41:27 +0200

    docs: add friendly description to settings

--- settings.py
+++ settings.py
@@ -1,9 +1,17 @@
+# Unifi settings
+# Credentials for API access
 UNIFI_USERNAME = 'admin'
 UNIFI_PASSWORD = 'belastend2342'
+# Specify Unifi site (id in URL path)
 UNIFI_SITE = 'default'
+# Unifi controller base URL
 UNIFI_URL = 'https://localhost:8443'
+# Unifi voucher parameters
 EXPIRE = 67  # in minutes
 USAGES = 35
+# Cups settings
 CUPS_SERVER = 'localhost'
+# Specify printer (friendly name is okay)
 CUPS_PRINTER = 'HP'
-BUTTON_PIN = 3  # Button pin
+# Raspberry PI button GPIO pin (GPIO, not PIN! Check pinout!)
+BUTTON_PIN = 3
